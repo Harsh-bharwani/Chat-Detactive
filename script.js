49cbb7c4082efb1d340b79e9877f5d3bd856b646
Harsh Bharwani <harshbharwani1234@gmail.com>
2025-07-19 20:00:57 +0530

    update: frontend and functionality ready

--- script.js
+++ script.js
@@ -37,10 +37,10 @@ function LPS(str){ // return the LPS array for the given string
 
 function KMP(pattern, sentence) {
     let str=pattern+'@'+sentence, m=pattern.length;    
-    console.log(str);
+    // console.log(str);
     
     LPSarray=LPS(str);
-    console.log(LPSarray);
+    // console.log(LPSarray);
     
     position=[];
     LPSarray.forEach((val, ind)=>{
@@ -54,30 +54,34 @@ function KMP(pattern, sentence) {
     return position;
 }
 
+function highlightedText(sentence, position, index){
+    let color=(index%2==0)? "yellow":"green";
+    let newSentence=sentence.slice(0, position[0]["start"]);
+    for(let i=0;i<position.length;i++){
+        newSentence+=sentence.slice(position[i]["start"], position[i]["end"] +1).fontcolor(color).bold();
+        if(i+1<position.length) newSentence+=sentence.slice(position[i]["end"]+1, position[i+1]["start"]);
+    }
+    newSentence+=sentence.slice(position[position.length-1]["end"]+1, sentence.length-1);
+    return newSentence;
+}
+
 function highlightFilteredChats(){
-    const word=search.value;
+    const word=search.value.trim();
+    updateData();
     if(word){
         msg.classList.remove("d-none");
+        chats.style.height="60vh";
         if(invertedIndex[word]==undefined) {
             msg.textContent=`No messages found containing "${word}"`;
-            // chats.children[index].children[1].textContent=sentence;
-
         }
         else{
             let wordFreq=0;
             for(let index of invertedIndex[word])
             {   
-                let sentence=chats.children[index].children[1].textContent;
+                let sentence=chats.children[index].children[1].innerHTML;   
                 let position=KMP(word, sentence.toLowerCase());  
-                // console.log(position);
-
-                let newSentence=sentence.slice(0, position[0]["start"]);
-                for(let i=0;i<position.length;i++){
-                    newSentence+=sentence.slice(position[i]["start"], position[i]["end"] +1).fontcolor("yellow");
-                    wordFreq++;
-                }
-                newSentence+=sentence.slice(position[position.length-1]["end"]+1, sentence.length-1);
-                chats.children[index].children[1].innerHTML=newSentence;
+                wordFreq+=position.length;
+                chats.children[index].children[1].innerHTML=highlightedText(sentence, position, index);
            }           
             msg.textContent=`${wordFreq} messages found containing "${word}"`;
         }
@@ -90,6 +94,12 @@ function highlightFilteredChats(){
     }
     tokenization();
 })()
+
+function updateData(){
+    messages.forEach((sentence, index)=>{
+        chats.children[index].children[1].innerHTML=sentence;
+    })
+}
 // console.log(chats.children[0].children[1].textContent);
 
 
